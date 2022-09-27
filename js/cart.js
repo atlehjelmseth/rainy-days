faa15443957d72d49d0e384b921c8e90f9ec5e1c
atlehjelmseth <atle.hjelmseth@stud.noroff.no>
2022-09-27 13:45:08 +0200

    Update

--- js/cart.js
+++ js/cart.js
@@ -1,10 +1,39 @@
 const cart = document.querySelectorAll(".add_to_cart");
 const clearCart = document.querySelectorAll(".clear_cart");
 
+/* Jackets */
 
+const jackets = [
+  {
+    name: 'Grey jacket',
+    tag: 'greyjacket',
+    price: '299',
+    inCart: '0'
+  },
+  {
+    name: 'Red jacket',
+    tag: 'redjacket',
+    price: '199',
+    inCart: '0'
+  },
+  {
+    name: 'Blue jacket',
+    tag: 'bluejacket',
+    price: '299',
+    inCart: '0'
+  },
+  {
+    name: 'Yellow jacket',
+    tag: 'greyjacket',
+    price: '299',
+    inCart: '0'
+  },
+]
+
+/* Add to cart */
 for (let i=0; i < cart.length; i++) {
   cart[i].addEventListener('click', () => {
-    cartNumber()
+    cartNumber(jackets[i])
   })
 }
 
@@ -12,28 +41,28 @@ function numberOfProducts() {
   let jacketNumber = localStorage.getItem('cartNumber');
 
   if(jacketNumber) {
-    document.querySelector('.fa-shopping-cart span').textContent = jacketNumber;
+    document.querySelector('.fa-shopping-cart p').textContent = jacketNumber;
   }
 }
 
-function cartNumber() {
+function cartNumber(jackets) {
+  console.log("The product is", jackets)
   let jacketNumber = localStorage.getItem('cartNumber');
 
   jacketNumber = parseInt(jacketNumber);
 
   if (jacketNumber){
     localStorage.setItem('cartNumber', jacketNumber + 1);
-    document.querySelector('.fa-shopping-cart span').textContent = jacketNumber + 1;
+    document.querySelector('.fa-shopping-cart p').textContent = jacketNumber + 1;
   } else {
     localStorage.setItem('cartNumber', 1);
-    document.querySelector('.fa-shopping-cart span').textContent = 1;
+    document.querySelector('.fa-shopping-cart p').textContent = 1;
   };
 }
 
-
-
-
 numberOfProducts()
+
+/* Clear cart */
 
 for (let i=0; i < clearCart.length; i++) {
   clearCart[i].addEventListener('click', () => {
@@ -43,5 +72,5 @@ for (let i=0; i < clearCart.length; i++) {
 
 function clearOut() {
   localStorage.clear();
-  document.querySelector('.fa-shopping-cart span').textContent = 0;
+  document.querySelector('.fa-shopping-cart p').textContent = 0;
 }
--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-This is a file created just to show the folder
+/* Hamburger menu */
+
+const dropdownMenu = document.querySelector('nav');
+const menuButton = document.querySelector('.fa-bars');
+
+
+let menuOpen = false;
+menuButton.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuButton.classList.add('open');
+    menuOpen = true;
+    dropdownMenu.style.display = "block";
+  } else {
+    menuButton.classList.remove('open');
+    menuOpen = false;
+    dropdownMenu.style.display = "none";
+  }
+});
